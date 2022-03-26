7bec8c19757572013e22ee3695ebc4118fc1fc21
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2022-03-26 13:58:50 +0000

    Starting project

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -13,6 +13,36 @@ const getState = ({ getStore, getActions, setStore }) => {
 					initial: "white"
 				}
 			]
+			,
+		data: {
+			gryffindor: [
+				{
+					"name": "Harry Potter",
+					"alternate_names": [],
+					"species": "human",
+					"gender": "male",
+					"house": "Gryffindor",
+					"dateOfBirth": "31-07-1980",
+					"yearOfBirth": 1980,
+					"wizard": true,
+					"ancestry": "half-blood",
+					"eyeColour": "green",
+					"hairColour": "black",
+					"wand": {
+						"wood": "holly",
+						"core": "phoenix feather",
+						"length": 11
+					},
+					"patronus": "stag",
+					"hogwartsStudent": true,
+					"hogwartsStaff": false,
+					"actor": "Daniel Radcliffe",
+					"alternate_actors": [],
+					"alive": true,
+					"image": "http://hp-api.herokuapp.com/images/harry.jpg"
+				}
+			]
+		}
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
